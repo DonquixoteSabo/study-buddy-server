654c08521b889d27362cf777d8d2d662b0272a11
DonquixoteSabo <mixboksu@gmail.com>
2022-03-14 18:14:55 +0100

    feat: Add students route
    Get specified student

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use('/students', studentsRouters);
 app.use('/groups', groupsRouters);
-
+ 
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
--- routes/students.js
+++ routes/students.js
@@ -1,9 +1,14 @@
 import express from 'express';
 
+import data from '../data/data.js';
+
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.json({ title: "you're on students routes" });
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  const matchingStudent = data.filter(student => student.id.$oid === id);
+
+  res.json(matchingStudent);
 });
 
 export default router;
