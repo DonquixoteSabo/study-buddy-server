d5881d3049235f69268cbb37c471d6a05bfb38c8
DonquixoteSabo <mixboksu@gmail.com>
2022-03-28 15:55:09 +0200

    feat: Connect database

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
 import express from 'express';
 import cors from 'cors';
+import mongoose from 'mongoose';
 
 import studentsRouters from './routes/students.js';
 import groupsRouters from './routes/groups.js';
+import notesRouters from './routes/notes.js';
 
 const app = express();
 
 app.use(cors());
 app.use('/students', studentsRouters);
+app.use('/notes', notesRouters);
 app.use('/groups', groupsRouters);
- 
+
 const PORT = process.env.PORT || 5000;
 
+// modify password and to env-variables
+const CONNECTION_URL = `mongodb+srv://teacherstudybuddy:HXMu2gHaFJY3IjOC@studybuddy.h2pht.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+mongoose
+  .connect(CONNECTION_URL)
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+  )
+  .catch(err => console.log(err.message));
+
 app.get('/', (req, res) => {
-  res.send("HEllo, it's me");
+  res.send("Hello, it's me");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is listening on ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server is listening on ${PORT}`);
+// });
